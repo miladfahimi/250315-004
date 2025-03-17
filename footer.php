a4544b4c17607a4bef2e61de859d5df7a96581d9
miladfah <milad.fahimi@hiq.se>
2025-03-17 20:38:05 +0330

    add more enhanced footer

--- footer.php
+++ footer.php
@@ -1,7 +1,23 @@
-<footer>
-    <p>© <?php echo date('Y'); ?> تمامی حقوق محفوظ است | تک تنیس</p>
-    <?php wp_footer(); ?>
-</footer>
-</body>
-</html>
+<footer class="footer">
 
+  <div class="about">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.jpg" alt="Tak Tennis Logo">
+    <p>گروه ورزشی تک تنیس در سال ۱۳۹۴ با نام تجاری کانال ورزش شروع به فعالیت در زمینه فروش لوازم ورزشی از قبیل
+      راکت‌های تنیس، اسکواش و بدمینتون و همچنین لوازم ورزشی زمستانی از قبیل آلپاین و اسنوبرد نموده و در سال ۱۳۹۸ با
+      تغییر نام تجاری خود به تک تنیس به فعالیت‌های خود ادامه داده است.</p>
+  </div>
+
+  <div class="social-info">
+    <p><i class="fas fa-phone"></i> تلفن: (+98)21 66977902</p>
+    <p><i class="fab fa-instagram"></i> اینستاگرام: <a href="#">TakTennis</a></p>
+    <p><i class="fab fa-telegram"></i> تلگرام: <a href="#">TakTenniss</a></p>
+    <p><i class="fas fa-envelope"></i> ایمیل: <a href="mailto:info@taktennis.com">info@taktennis.com</a></p>
+  </div>
+  <div class="contact-info">
+    <p>شعبه اول: تهران، خیابان ولیعصر، میدان منیریه، کوچه فردوس، مجتمع تجاری کاوه، طبقه اول، واحد ۴۸</p>
+    <p>شعبه دوم: تهران، شهرک غرب، خیابان ایران زمین، مجتمع تجاری گلستان، طبقه اول، واحد ۲۹۹</p>
+  </div>
+</footer>
+<div class="footer-bottom">
+  تمام حقوق وبسایت برای مجموعه تک تنیس محفوظ است. © Copyright ۲۰۲۴
+</div>

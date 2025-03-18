b550360cf5263456c5aa39817a1d65e97b958ce5
miladfah <milad.fahimi@hiq.se>
2025-03-18 09:36:56 +0330

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cd /home/nisateamir/public_html/taktennis/wp-content/themes/tak-tennis-theme
-git reset --hard origin/master
-git pull origin master
+
+# Navigate to the correct theme directory
+cd /home/nisateamir/public_html/taktennis/wp-content/themes/tak-tennis-theme || exit
+
+# Pull the latest changes from GitHub
+git pull origin main > deploy.log 2>&1
+
+# Set correct file permissions (optional)
+chown -R nisateamir:nisateamir /home/nisateamir/public_html/taktennis/wp-content/themes/tak-tennis-theme
+chmod -R 755 /home/nisateamir/public_html/taktennis/wp-content/themes/tak-tennis-theme
+
+# Log the deployment
+echo "Deployed at $(date)" >> deploy.log
